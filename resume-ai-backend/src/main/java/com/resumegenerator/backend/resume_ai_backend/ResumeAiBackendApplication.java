48c855cb0f57c86866dd5ac2e5d1d4fb69be4a47
Suyash Baoney <suyashbaoney58@gmail.com>
2025-05-19 14:18:43 +0530

    Setting up backend

---
+++ resume-ai-backend/src/main/java/com/resumegenerator/backend/resume_ai_backend/ResumeAiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.resumegenerator.backend.resume_ai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeAiBackendApplication.class, args);
+	}
+
+}
